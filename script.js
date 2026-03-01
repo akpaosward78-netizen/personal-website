900a462dbec8ef54de187965a870e2044c042306
akpaosward78-netizen <akpaosward78@gmail.com>
2026-03-01 01:31:48 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ function showMessage() {
         "You can contact me at: osward@email.com";
         
 }
+const gallery = document.querySelector('.gallery');
+const images = ['image1.jpg', 'image2.jpg', /* more images */];
+
+images.forEach(src => {
+  const img = document.createElement('img');
+  img.src = src;
+  gallery.appendChild(img);
+});
